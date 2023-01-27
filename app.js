211ffa364676107884b0e13b94922d0fd9011edf
Faizan Sayed <baa98989@gmail.com>
2023-01-26 21:01:20 -0500

    first commit

--- app.js
+++ app.js
@@ -1,13 +1,43 @@
-//set up the server
-const express = require( "express" );
+// //set up the server
+// const express = require( "express" );
+// const logger = require("morgan");
+// const app = express();
+// const port = 8080;
+
+// // define middleware that logs all incoming requests
+// app.use(logger("dev"));
+
+// // define middleware that serves static resources in the public directory
+// app.use(express.static(__dirname + '/public'));
+
+// // define a route for the default home page
+// app.get( "/", ( req, res ) => {
+//     res.sendFile( __dirname + "/views/index.html" );
+// } );
+
+// // define a route for the stuff inventory page
+// app.get( "/stuff", ( req, res ) => {
+//     res.sendFile( __dirname + "/views/stuff.html" );
+// } );
+
+// // define a route for the item detail page
+// app.get( "/stuff/item", ( req, res ) => {
+//     res.sendFile( __dirname + "/views/item.html" );
+// } );
+
+// // start the server
+// app.listen( port, () => {
+//     console.log(`App server listening on ${ port }. (Go to http://localhost:${ port })` );
+// } );
+
+// //set up the server
 const logger = require("morgan");
+const express = require( "express" );
 const app = express();
 const port = 8080;
 
 // define middleware that logs all incoming requests
 app.use(logger("dev"));
-
-// define middleware that serves static resources in the public directory
 app.use(express.static(__dirname + '/public'));
 
 // define a route for the default home page
@@ -25,7 +55,7 @@ app.get( "/stuff/item", ( req, res ) => {
     res.sendFile( __dirname + "/views/item.html" );
 } );
 
-// start the server
+// // start the server
 app.listen( port, () => {
     console.log(`App server listening on ${ port }. (Go to http://localhost:${ port })` );
 } );
